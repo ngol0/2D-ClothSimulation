10d6dbb6937969c77a950c57ec1ad3cbb52438b0
lampngo <lamngo.251@gmail.com>
2024-02-07 09:55:41 +0000

    clean up

--- ClothSimulation/src/global/utils.cpp
+++ ClothSimulation/src/global/utils.cpp
@@ -2,13 +2,6 @@
 
 namespace utils
 {
-	float distance_point(const particle& p1, const particle& p2)
-	{
-		float dx = p1.pos.x - p2.pos.x;
-		float dy = p1.pos.y - p2.pos.y;
-		return sqrt(dx * dx + dy * dy);
-	}
-
 	float vector_length(const vec2& v)
 	{
 		return sqrtf(v.x * v.x + v.y * v.y);
--- ClothSimulation/src/global/utils.h
+++ ClothSimulation/src/global/utils.h
@@ -1,11 +1,9 @@
 #pragma once
-#include "rect/particle.h"
 #include "vec2.h"
 #include <math.h>
 
 namespace utils
 {
-	float distance_point(const particle& p1, const particle& p2);
 	float vector_length(const vec2& v);
 	float squared_vector_length(const vec2& v);
 }
